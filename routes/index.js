71465a696b51966418cd7ed79abf3cf5c6d82153
kinokotakenoko2525 <160029380+kinokotakenoko2525@users.noreply.github.com>
2024-07-04 07:31:25 +0000

    15章password入力後エラー

--- routes/index.js
+++ routes/index.js
@@ -1,49 +1,50 @@
 const express = require('express');
 const router = express.Router();
 const knex = require('../db/knex');
-const pg = require('pg');
+//const pg = require('pg');
 
-const connection = new pg.Pool({
-  host: 'localhost',
-  user: 'root',
-  password: 'postgres',
-  database: 'todo_app',
-  port: 5432,
-  })
+//const connection = new pg.Pool({
+  //host: 'localhost',
+  //user: 'root',
+  //password: 'postgres',
+  //database: 'todo_app',
+  //port: 5432,
+  //})
 
 
-router.get('/', function (req, res, next) {
-  knex("tasks")
-    .select("*")
-    .then(function (results) {
-      console.log(results);
-      res.render('index', {
-        title: 'ToDo App',
-        todos: results,
+  router.get('/', function (req, res, next) {
+    knex("tasks")
+      .select("*")
+      .then(function (results) {
+        console.log(results);
+        res.render('index', {
+          title: 'ToDo App',
+          todos: results,
+        });
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.render('index', {
+          title: 'ToDo App',
+        });
       });
-    })
-    .catch(function (err) {
-      console.error(err);
-      res.render('index', {
-        title: 'ToDo App',
+  });
+  
+  router.post('/', function (req, res, next) {
+    const todo = req.body.add;
+    knex("tasks")
+      .insert({user_id: 1, content: todo})
+      .then(function () {
+        res.redirect('/')
+      })
+      .catch(function (err) {
+        console.error(err);
+        res.render('index', {
+          title: 'ToDo App',
+        });
       });
- });
-});
-
-router.post('/', function (req, res, next) {
-  const todo = req.body.add;
-  knex("tasks")
-    .insert({user_id: 1, content: todo})
-    .then(function () {
-      res.redirect('/')
-    })
-    .catch(function (err) {
-      console.error(err);
-      res.render('index', {
-        title: 'ToDo App',
-      });
-    });
-});
-
-
-module.exports = router;
+  });
+  
+  router.use('/signup', require('./signup'));
+  
+  module.exports = router;
